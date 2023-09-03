b705c875e6d98d6adc05c9b6c5c2087cba76ca78
Luminous_Llama <keyushattarde@gmail.com>
2023-09-03 03:15:12 -0700

    dune fmt things 2

--- tests/Units/arithmetic/arithmetic.ml
+++ tests/Units/arithmetic/arithmetic.ml
@@ -1,4 +1,7 @@
 external add : int -> int -> int = "arithmetic_add"
 external subtract : int -> int -> int = "arithmetic_subtract"
 external multiply : int -> int -> int = "arithmetic_multiply"
-external divide : int -> int -> (float [@unboxed]) = "arithmetic_divide_bytecode" "arithmetic_divide_native" [@@noalloc]
+
+external divide : int -> int -> (float[@unboxed])
+  = "arithmetic_divide_bytecode" "arithmetic_divide_native"
+  [@@noalloc]

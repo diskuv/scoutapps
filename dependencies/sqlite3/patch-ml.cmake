e0e6858570e104f673ea3ba90e06863ae5fbfd75
Jonah Beckford <9566106-jonahbeckford@users.noreply.gitlab.com>
2023-10-03 22:05:54 -0700

    Fix sqlite3_stubs.c issues 54

--- dependencies/sqlite3/patch-ml.cmake
+++ dependencies/sqlite3/patch-ml.cmake
@@ -20,3 +20,13 @@ endif()
 
 configure_file("${PATCHDIR}/discover.ml" "${DESTDIR}/src/config/discover.ml" COPYONLY)
 configure_file("${PATCHDIR}/src.dune" "${DESTDIR}/src/dune" COPYONLY)
+
+# Fix for https://github.com/mmottl/sqlite3-ocaml/issues/54
+#   Move the following: #include <sqlite3.h>
+#   to the top of the file.
+file(READ "${DESTDIR}/src/sqlite3_stubs.c" sqlite3_stubs_c_CONTENT)
+if(NOT sqlite3_stubs_c_CONTENT MATCHES "patch-ml POST[.]EDIT[.]1") # idempotent
+    string(REPLACE "#include <sqlite3.h>" "/* patch-ml PRE.EDIT.1: #include <sqlite3.h> */" sqlite3_stubs_c_CONTENT "${sqlite3_stubs_c_CONTENT}")
+    string(PREPEND sqlite3_stubs_c_CONTENT "#include <sqlite3.h> /* patch-ml POST.EDIT.1 */\n")
+    file(WRITE "${DESTDIR}/src/sqlite3_stubs.c" "${sqlite3_stubs_c_CONTENT}")
+endif()

43df5b1be853443484e44a4b8ef71e0076dc757f
Jonah Beckford <9566106-jonahbeckford@users.noreply.gitlab.com>
2023-10-21 01:05:49 -0700

    Add backtrace

--- src/ObjsLib/SquirrelScout_ObjsLib.ml
+++ src/ObjsLib/SquirrelScout_ObjsLib.ml
@@ -1,3 +1,6 @@
+(* Always make sure there is a full backtrace available *)
+let () = Printexc.record_backtrace true
+
 (* Automatically register the COM objects when the COM system is initialized *)
 let () = DkSDKFFIOCaml_Std.ComEvents.add_init_event_handler (fun () ->
   let com = DkSDKFFIOCaml_Std.Com.create_c () in

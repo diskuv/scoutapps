777b6f5b46481ec92a3fb68e6209963b365334b5
ArcCreate <arckumar12@gmail.com>
2024-01-23 15:10:57 -0800

    trial commit

--- app/src/main/java/com/example/squirrelscout_scouter/MainActivity.java
+++ app/src/main/java/com/example/squirrelscout_scouter/MainActivity.java
@@ -22,6 +22,7 @@ import com.example.squirrelscout_scouter.ui.viewmodels.ScoutingSessionViewModel;
 
 import java.util.Locale;
 
+//trial comment 2024
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Handler uiThreadHandler;
 

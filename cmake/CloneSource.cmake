3b43e80390ab0576de50e8717500b1c3939e3a8d
Jonah Beckford <9566106-jonahbeckford@users.noreply.gitlab.com>
2024-01-27 00:09:17 -0800

    Add cmake/CloneSource.cmake

---
+++ cmake/CloneSource.cmake
@@ -0,0 +1,47 @@
+# TLDR: Consider this to be similar to .gitignore for the entire project.
+#
+# Summary: Clone the source code of this project.
+#
+# When: Used when another project uses this project with
+# DkSDKFetchContent_DeclareButAllowLocalOverride(). For example, another project may
+# use this project (pretend it is called "This_Project") with the following:
+#
+#     DkSDKFetchContent_DeclareButAllowLocalOverride(
+#         NAME ocaml-backend
+#         GIT_REPOSITORY "https://github.com/SquirrelScout/ocaml-backend.git"
+#         GIT_TAG teamNamesTable)
+#     DkSDKFetchContent_MakeAvailableNoInstall(ocaml-backend)
+#
+# How: This script will be run as a
+# `cmake -D CMAKE_INSTALL_PREFIX=... -P cmake/CloneSource.cmake` command from
+# the base of this project (the directory that has ./dk, ./dk.cmd and dune-project).
+#
+# Consequences: Without this script present ALL of _this_ project is copied
+# into the build folder of _that_ project. Which can be INCREDIBLY
+# TIME-CONSUMING when you have huge build folders and so on in this project.
+#
+# Suggestions?
+#   Ignore test folders. Why does another project need to run your tests?
+#
+#   Do not add CMakeLists.txt. Only the source code necessary for Dune is needed.
+#
+#   See https://cmake.org/cmake/help/latest/command/file.html#copy-file for
+#   all the options you can use.
+#
+#   For debugging, you should use file(INSTALL ...) rather than file(COPY ...)
+#   so you can see what is being copied.
+
+if(NOT CMAKE_INSTALL_PREFIX)
+    message(FATAL_ERROR "You cannot run this script without setting CMAKE_INSTALL_PREFIX")
+endif()
+
+file(INSTALL
+     # Directories (don't use trailing slash)
+     src data dependencies opam
+     # Dune files
+     _all_cmake.dune dune dune-project
+     # CMake files
+     CMakeLists.txt CMakePresets.json CMakeUserPresets.json
+     DESTINATION "${CMAKE_INSTALL_PREFIX}"
+     NO_SOURCE_PERMISSIONS
+     FOLLOW_SYMLINK_CHAIN)

9a263343e50c5cd70c953ce7a21a328a259f6b69
Jonah Beckford <9566106-jonahbeckford@users.noreply.gitlab.com>
2024-07-25 21:48:16 -0700

    Install winget if needed

--- src/SonicScout_Setup/Winget.ml
+++ src/SonicScout_Setup/Winget.ml
@@ -1,10 +1,27 @@
 open Utils
 
+let install_winget () =
+  let open Bos in
+  Logs.info (fun l -> l "Installing winget");
+  OS.Cmd.run
+    Cmd.(v (Filename.concat (Tr1Assets.LocalDir.v ()) "install-winget.cmd"))
+  |> rmsg
+
 let install args =
   let open Bos in
+  (* Install winget if we can't locate it. *)
+  let winget_cmd = Cmd.(v "winget") in
+  let winget_cmd =
+    match OS.Cmd.find_tool winget_cmd |> rmsg with
+    | Some winget_exe -> Cmd.(v (p winget_exe))
+    | None ->
+        install_winget ();
+        winget_cmd
+  in
+  (* Do `winget install xyz` *)
   Logs.info (fun l ->
       l "winget install %a" (Fmt.list ~sep:Fmt.sp Fmt.string) args);
-  match OS.Cmd.run_status Cmd.(v "winget" % "install" %% of_list args) with
+  match OS.Cmd.run_status Cmd.(winget_cmd % "install" %% of_list args) with
   | Ok (`Exited 0) -> ()
   | Ok (`Exited -1978335189) ->
       (*Found an existing package already installed.*) ()
---
+++ src/SonicScout_Setup/assets__/install-winget.ps1
@@ -0,0 +1,17 @@
+# https://learn.microsoft.com/en-us/windows/package-manager/winget/#install-winget-on-windows-sandbox
+
+# https://github.com/microsoft/terminal/issues/280#issuecomment-1728298632
+# This happens in Windows Sandbox
+# (also see install-winget.cmd)
+[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+
+$progressPreference = 'silentlyContinue'
+Write-Information "Downloading WinGet and its dependencies..."
+Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
+Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.8.6/Microsoft.UI.Xaml.2.8.x64.appx -OutFile Microsoft.UI.Xaml.2.8.x64.appx
+Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
+Add-AppxPackage Microsoft.UI.Xaml.2.8.x64.appx
+Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+
+$progressPreference = 'Continue'

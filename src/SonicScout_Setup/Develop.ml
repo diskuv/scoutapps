1db6f9400cbc455b34b48ee4ffda43853deabc96
Jonah Beckford <9566106-jonahbeckford@users.noreply.gitlab.com>
2024-07-28 13:18:38 -0700

    Skip building Android when just building the scanner

--- src/SonicScout_Setup/Develop.ml
+++ src/SonicScout_Setup/Develop.ml
@@ -4,7 +4,7 @@ type common = {
   global_dkml : bool;
 }
 
-let compile_base { dksdk_data_home; opts; global_dkml } =
+let compile_base ?skip_android { dksdk_data_home; opts; global_dkml } =
   let global_dkml = if global_dkml then Some () else None in
 
   InitialSteps.run ~dksdk_data_home ();
@@ -13,7 +13,11 @@ let compile_base { dksdk_data_home; opts; global_dkml } =
   let slots = Slots.create () in
   let slots = DkML.run ?global_dkml ~slots () in
   let slots = ScoutBackend.run ?global_dkml ~opts ~slots () in
-  let slots = ScoutAndroid.run ~opts ~slots () in
+  let slots =
+    match skip_android with
+    | Some () -> slots
+    | None -> ScoutAndroid.run ~opts ~slots ()
+  in
   slots
 
 let compile common =
@@ -33,7 +37,7 @@ let launch_android common =
 
 let launch_scanner common =
   try
-    let slots = compile_base common in
+    let slots = compile_base ~skip_android:() common in
     let slots = Scanner.run ~slots () in
     ignore slots;
     Utils.done_steps "Developing"
@@ -80,7 +84,8 @@ module Cli = struct
     let open SSCli in
     let doc =
       "Launch the QR code scanner. Your machine will be setup with \
-       prerequisites, and code will be compiled, if it hasn't been already."
+       prerequisites, and code will be compiled (everything except Android), \
+       if it hasn't been already."
     in
     let man = [ `S Manpage.s_description; `Blocks help_secs ] in
     Cmd.v (Cmd.info ~doc ~man "scanner") Term.(const launch_scanner $ common_t)
